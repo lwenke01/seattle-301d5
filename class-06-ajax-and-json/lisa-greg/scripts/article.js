7588a25e6070fb51461ffaf1973c3bc6685e204a
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-08 15:03:54 -0800

    finished stretch goal

--- class-06-ajax-and-json/lisa-greg/scripts/article.js
+++ class-06-ajax-and-json/lisa-greg/scripts/article.js
@@ -7,11 +7,6 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-// DONE: Instead of a global `articles = []` array, let's track this list of all articles directly on the
-// constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves
-// objects, which means we can add properties/values to them at any time. In this case, we have
-// a key/value pair to track, that relates to ALL of the Article objects, so it does not belong on
-// the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 Article.prototype.toHtml = function() {
@@ -24,13 +19,6 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// DONE: There are some other functions that also relate to articles across the board, rather than
-// just single instances. Object-oriented programming would call these "class-level" functions,
-// that are relevant to the entire "class" of objects that are Articles.
-
-// DONE: This function will take the rawData, how ever it is provided,
-// and use it to instantiate all the articles. This code is moved from elsewhere, and
-// encapsulated in a simply-named function for clarity.
 Article.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -41,14 +29,8 @@ Article.loadAll = function(rawData) {
   })
 }
 
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
 Article.fetchAll = function() {
   if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // we can load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the articleView object).
-
     $.ajax({
       type: 'HEAD',
       url: 'data/hackerIpsum.json',
@@ -56,7 +38,9 @@ Article.fetchAll = function() {
         console.log('xhr is ' + xhr);
         var eTag = xhr.getResponseHeader('eTag');
         if (!localStorage.eTag || eTag !== localStorage.eTag) {
+          localStorage.clear();
           localStorage.setItem('eTag' , JSON.stringify(eTag));
+          Article.fetchAll();
         } else {
         Article.loadAll(JSON.parse(localStorage.rawData));
         }
@@ -64,13 +48,7 @@ Article.fetchAll = function() {
     });
       Article.loadAll(JSON.parse(localStorage.rawData));
       articleView.initIndexPage();
-      console.log(); //TODO: What method do we call to render the index page?
   } else {
-    // TODO: When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Article.all with the .loadAll function above,
-    // and then render the index page.
     $.ajax({
       type: 'GET',
       url: 'data/hackerIpsum.json',
@@ -78,10 +56,7 @@ Article.fetchAll = function() {
         var eTag = xhr.getResponseHeader('eTag');
         localStorage.setItem('rawData', JSON.stringify(data));
         Article.loadAll(data);
-        console.log('data is ' + data);
       }
-
-
   });
 }
 }
