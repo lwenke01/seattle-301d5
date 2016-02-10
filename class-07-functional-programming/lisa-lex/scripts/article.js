7484d9dadd374553f034f4e082cdc6f592ac798e
lwenke01 <lwenke@gmail.com>
2016-02-09 16:12:46 -0800

    finished - got everything to work. yay.

--- class-07-functional-programming/lisa-lex/scripts/article.js
+++ class-07-functional-programming/lisa-lex/scripts/article.js
@@ -48,9 +48,9 @@ Article.loadAll = function(rawData) {
 
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
-// TODO: Refactor this function, so it accepts an argument of a callback function (likely a view function)
+// DONE: Refactor this function, so it accepts an argument of a callback function (likely a view function)
 // to execute once the loading of articles is done.
-Article.fetchAll = function() {
+Article.fetchAll = function(module) {
   if (localStorage.rawData) {
     Article.loadAll(JSON.parse(localStorage.rawData));
     articleView.initIndexPage();
@@ -66,7 +66,6 @@ Article.fetchAll = function() {
 // DONE: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
 Article.numWordsAll = function() {
   return Article.all.map(function(article) {
-    console.log(article.body.match(/\b\w+/g).length);
     return article.body.match(/\b\w+/g).length;
   })
   .reduce(function(a, b) {
